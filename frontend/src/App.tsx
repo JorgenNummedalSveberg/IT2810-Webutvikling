b4f408911da770761d1a73a454167f1719b3ae56
Jørgen Nummedal Sveberg <jorgenns@stud.ntnu.no>
2020-10-20 12:53:08 +0200

    added basic card based semantic ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,49 @@ import React from 'react';
 import './CSS/App.css';
 import Header from "./Components/Header";
 import MainContent from './Components/MainContent';
+import 'semantic-ui-css/semantic.min.css'
+import {Card, Icon, Image} from 'semantic-ui-react';
+import _ from 'lodash';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <MainContent/>
+      <header className="App-header">
+          <Card.Group className={"CardGroup"} centered>
+              {cards}
+          </Card.Group>
+      </header>
     </div>
   );
+}
+
+const cards = _.times(10, (i: number) => (
+    <MovieCard key={i}/>
+))
+
+function MovieCard() {
+  return(
+      <Card className={"movieCard"}>
+        <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>Matthew</Card.Header>
+          <Card.Meta>
+            <span className='date'>Joined in 2015</span>
+          </Card.Meta>
+          <Card.Description>
+            Matthew is a musician living in Nashville.
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <a>
+            <Icon name='user' />
+            22 Friends
+          </a>
+        </Card.Content>
+      </Card>
+  )
 }
 
 export default App;
