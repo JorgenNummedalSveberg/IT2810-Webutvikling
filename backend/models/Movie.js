6c4e51876aedf378835516a7781b32549c11f181
Jørgen Nummedal Sveberg <jorgenns@stud.ntnu.no>
2020-10-20 16:00:39 +0200

    changed movies model genre to [String]

--- backend/models/Movie.js
+++ backend/models/Movie.js
@@ -4,7 +4,7 @@ const schema = mongoose.Schema({
     id: String,
     title: String,
     year: String,
-    genres: Array,
+    genres: [String],
     ratings: Array,
     poster: String,
     contentRating: String,
