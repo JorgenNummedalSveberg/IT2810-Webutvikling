f1f238c5d3fce7b8ad789e6c58b4b6f29ff7e17a
Jørgen Nummedal Sveberg <jorgenns@stud.ntnu.no>
2020-10-23 12:23:57 +0200

    this should be working

--- backend/routes.js
+++ backend/routes.js
@@ -3,15 +3,18 @@ const Movie = require("./models/Movie");
 const router = express.Router();
 
 router.get("/movies/:genre/:title", async (req, res,e) => {
+    let movies;
     try{
         console.log(req.params.genre.substring(7));
         console.log(req.params.title.substring(7));
-        const movie = await Movie.find({'genre': req.params.genre})
-            .find({ 'title': { $regex: req.params.title.substring(6), $options: "i" } });
+        const movie = Movie
+            .find({'genres': req.params.genre.substring(7)})
+            .find({ 'title': { $regex: req.params.title.substring(7), $options: "i" } })
         console.log(movie);
         res.send(movie);
+        movies = movie;
     } catch(e){
-        res.status(404).json({message: e})
+        res.status(404).json(e);
     }
 });
 
