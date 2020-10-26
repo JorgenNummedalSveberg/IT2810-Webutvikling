2adc8daf3f0b05341392fd17e538994d640e07eb
Jørgen Nummedal Sveberg <jorgenns@stud.ntnu.no>
2020-10-26 15:51:29 +0100

    Apply suggestion to frontend/src/Components/Header.tsx

--- frontend/src/Components/Header.tsx
+++ frontend/src/Components/Header.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import './CSS/Header.css';
 import './CSS/SearchField.css';
 import SortingPanel from "./SortingPanel";
