fa7281cf174825f5192cbbc86659db3f28166ec5
Jørgen Nummedal Sveberg <jorgenns@stud.ntnu.no>
2020-10-29 17:46:36 +0100

    moved MovieCard into its own folder and added stretched to make them all the same height

--- frontend/src/Components/GridView.tsx
+++ frontend/src/Components/GridView.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './CSS/GridView.css';
 import {Card, Icon, Grid, Image, Pagination, Dimmer, Loader} from 'semantic-ui-react';
-import ImdbIcon from "./ImdbIcon";
 import {useDispatch, useSelector} from "react-redux";
 import {state} from "../types/state";
 import {setPage, setPopup, showPopup} from "../actions";
 import Popup from './Popup';
-import {Movie} from "../types/Movie";
+import MovieCard from "./MovieCard";
 
 
 // Komponent som viser frem alle filmene i en responsiv grid
@@ -80,55 +79,6 @@ function GridView() {
             {pagination}
         </div>
     )
-}
-
-// Komponent for å vise frem en film
-function MovieCard(props: {movie: Movie}) {
-
-    // Nødvendig for redux
-    const dispatch = useDispatch();
-
-    // Åpner opp Popup.
-    function handleClick() {
-        dispatch(setPopup(props.movie));
-        dispatch(showPopup(true));
-    }
-
-    return(
-        <Grid.Column tablet={5} mobile={8} computer={3}>
-            <Card className={"movieCard"} style={{backgroundColor: '#464646'}} onClick={handleClick}>
-                <Image src={props.movie.posterurl} wrapped ui={false}/>
-                <Card.Content>
-                    <Card.Header id={"id_"+(props.movie.title).replace(/\s/g, "")} style={{color: 'white'}}>{props.movie.title}</Card.Header>
-                    <Card.Description style={{color: '#e5dfca'}}>
-                        <div role={"showGenre"}> Genres: {props.movie.genres.join(", ")} </div>
-                        <div id={"year_"+(props.movie.title).replace(/\s/g, "")}> Year: {props.movie.year} </div>
-                    </Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                    <Grid centered style={{margin: '5px'}}>
-                        <div style={{color: '#e5dfca', margin: 'auto'}}>
-                            <Icon size='large' name='hourglass'/>
-                            {parseTime(props.movie.duration)}
-                        </div>
-                        <ImdbIcon rating={props.movie.imdbRating} height={35}/>
-                    </Grid>
-                </Card.Content>
-            </Card>
-        </Grid.Column>
-
-    )
-}
-
-// Tar inn tid i formatet på databasen og gjør det om til presentabel string, eller rein minuttverdi
-function parseTime(time: number): string {
-    let minutes = time;
-    let hours = 0;
-    while (minutes-60 > 0) {
-        minutes -= 60;
-        hours++;
-    }
-    return hours+'h'+minutes+'m';
 }
 
 function DimCard() {
---
+++ frontend/src/Components/MovieCard.tsx
@@ -0,0 +1,57 @@
+// Komponent for å vise frem en film
+import {Movie} from "../types/Movie";
+import {useDispatch} from "react-redux";
+import {setPopup, showPopup} from "../actions";
+import {Card, Grid, Icon, Image} from "semantic-ui-react";
+import ImdbIcon from "./ImdbIcon";
+import React from "react";
+
+function MovieCard(props: {movie: Movie}) {
+
+    // Nødvendig for redux
+    const dispatch = useDispatch();
+
+    // Åpner opp Popup.
+    function handleClick() {
+        dispatch(setPopup(props.movie));
+        dispatch(showPopup(true));
+    }
+
+    return(
+        <Grid.Column stretched tablet={5} mobile={8} computer={3}>
+            <Card className={"movieCard"} style={{backgroundColor: '#464646'}} onClick={handleClick}>
+                <Image src={props.movie.posterurl} wrapped ui={false}/>
+                <Card.Content>
+                    <Card.Header id={"id_"+(props.movie.title).replace(/\s/g, "")} style={{color: 'white'}}>{props.movie.title}</Card.Header>
+                    <Card.Description style={{color: '#e5dfca'}}>
+                        <div role={"showGenre"}> Genres: {props.movie.genres.join(", ")} </div>
+                        <div id={"year_"+(props.movie.title).replace(/\s/g, "")}> Year: {props.movie.year} </div>
+                    </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    <Grid centered style={{margin: '5px'}}>
+                        <div style={{color: '#e5dfca', margin: 'auto'}}>
+                            <Icon size='large' name='hourglass'/>
+                            {parseTime(props.movie.duration)}
+                        </div>
+                        <ImdbIcon rating={props.movie.imdbRating} height={35}/>
+                    </Grid>
+                </Card.Content>
+            </Card>
+        </Grid.Column>
+
+    )
+}
+
+// Tar inn tid i formatet på databasen og gjør det om til presentabel string, eller rein minuttverdi
+function parseTime(time: number): string {
+    let minutes = time;
+    let hours = 0;
+    while (minutes-60 > 0) {
+        minutes -= 60;
+        hours++;
+    }
+    return hours+'h'+minutes+'m';
+}
+
+export default MovieCard
