0cebf33cd3f89272c198ae398f302f1ecf22101e
Jørgen Nummedal Sveberg <jorgenns@stud.ntnu.no>
2020-10-29 17:25:20 +0100

    Gave Grid a width so it doesn't squish the movie cards when pagination gets small

--- frontend/src/Components/GridView.tsx
+++ frontend/src/Components/GridView.tsx
@@ -74,7 +74,7 @@ function GridView() {
                 <Popup/> : null
             }
             {pagination}
-            <Grid style={{margin: "20px"}} centered>
+            <Grid style={{margin: "20px", width: '100%'}} centered>
                 {movieCards}
             </Grid>
             {pagination}
