fb884c3aec2641f7998143a6d781978a7b14ae47
Jørgen Nummedal Sveberg <jorgenns@stud.ntnu.no>
2020-11-11 18:40:07 +0100

    starting updating tests to material ui #7 #10

--- frontend/src/__tests__/Header.test.tsx
+++ frontend/src/__tests__/Header.test.tsx
@@ -12,7 +12,7 @@ function mock() {
 describe("Header only testing searchbar, sortbuttons have its own file", () => {
     test("Check if searchbar renders, and typing works", () => {
         const container = render(<Provider store={store}><Header refresh={mock}/></Provider>)
-        const as = container.getByRole('searcher')
+        const as = container.getByTestId('searcher')
         expect(as).toBeInTheDocument()
         userEvent.type(as, "This is a test input")
         expect(as).toHaveValue("This is a test input")
