9abe25ab7d554d2a9a761bbe653c394053c0a253
Jørgen Nummedal Sveberg <jorgenns@stud.ntnu.no>
2020-11-15 04:31:53 +0100

    removed useeffect import #2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useCallback, useState} from 'react';
 import Header from "./Components/Header/Header";
 import {useDispatch, useSelector} from "react-redux";
 import {setGenresState, setMovieState} from "./actions";
