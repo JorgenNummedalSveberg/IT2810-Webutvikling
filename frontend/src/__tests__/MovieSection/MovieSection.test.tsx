08343abb8ac83b9993abdea8ab698c44ecd7eaad
Jørgen Nummedal Sveberg <jorgenns@stud.ntnu.no>
2020-11-15 21:57:09 +0100

    updated tests #10 #12

--- frontend/src/__tests__/MovieSection/MovieSection.test.tsx
+++ frontend/src/__tests__/MovieSection/MovieSection.test.tsx
@@ -3,8 +3,21 @@ import {render} from '@testing-library/react';
 import MovieSection from '../../Components/MovieSection/MovieSection'
 import {Provider} from 'react-redux'
 import {store} from '../../reducers/store'
+import {makeStyles} from "@material-ui/styles";
+
+
+// Fake styles to trick components
+export function mock() {
+    const classes = makeStyles(
+        {
+            card:{}, gridItem: {}, paperButton: {}, paper: {}, poster: {},details: {},
+            title: {}, description: {}, bottomInfo: {}, duration: {}, noMargin: {}
+        }
+    )
+    return classes
+}
 
 test("Snapshot test", () => {
-    const movieSection = render(<Provider store={store}>(<MovieSection/></Provider>);
+    const movieSection = render(<Provider store={store}>(<MovieSection error={false} refresh={mock}/></Provider>);
     expect(movieSection).toMatchSnapshot();
 })
