c9f8cf5725e25b60745d66ed74b530b816ab6ad6
Jørgen Nummedal Sveberg <jorgenns@stud.ntnu.no>
2020-11-17 18:57:57 +0100

    fixed api call tests

--- frontend/cypress/integration/APItestCall_spec.tsx
+++ frontend/cypress/integration/APItestCall_spec.tsx
@@ -10,7 +10,7 @@ describe("Simulating API calls, and checks if the status response is correct. An
         cy.request({
             method: 'GET',
             url: 'http://localhost:5000/api/user?userName=test&password=pwd',
-        }).then(function (res) {
+        }).then(res => {
             expect(res.body).to.have.length(10)
         })
         cy.request({
@@ -23,22 +23,51 @@ describe("Simulating API calls, and checks if the status response is correct. An
             headers: {
                 'Content-Type': 'application/json'
             }
-        }).then(function (res) {
+        }).then(res => {
             expect(res.status).to.equal(200);
         })
     })
+
     it('A test call to search using title and genre ', () => {
+        const braveHeartBody = {
+            genre: '',
+            title: 'Braveheart',
+            sort: 'Name',
+            desc: true,
+            yearRange: [1900, 2020],
+            scoreRange: [0, 10],
+            user: '',
+            page: 0
+        }
+        const familyBody = {
+            genre: 'Family',
+            title: '',
+            sort: 'Name',
+            desc: true,
+            yearRange: [1900, 2020],
+            scoreRange: [0, 10],
+            user: '',
+            page: 0
+        }
         cy.request({
-            method: 'GET',
-            url: 'http://localhost:5000/api/movies?genre&title=braveheart',
-        }).then(function (res) {
-            expect(res.body[0].title).to.deep.equal('Braveheart')
+            method: 'POST',
+            url: 'http://localhost:5000/api/movies/index',
+            body: braveHeartBody
+        }).then(res => {
+            cy.request({
+                method: 'POST',
+                url: 'http://localhost:5000/api/movies/',
+                body: {ids: res.body.movies}
+            }).then(res => {
+                expect(res.body[0].title).to.deep.equal('Braveheart')
+            })
         })
         cy.request({
-            method: 'GET',
-            url: 'http://localhost:5000/api/movies?genre=Thriller&title=',
-        }).then(function (res) {
-            expect(res.body.length).to.deep.equal(37)
+            method: 'POST',
+            url: 'http://localhost:5000/api/movies/index',
+            body: familyBody
+        }).then(res => {
+            expect(res.body.movies.length).to.equal(13)
         })
     })
 })
